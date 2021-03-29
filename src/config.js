596dc72b548b3998ebf1f73c66a9071f24cfe6a6
Awesome Henry <henryebomah@gmail.com>
2021-03-29 12:08:09 -0700

    Delete src directory

--- src/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 8080,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://kj@localhost/jobstardb',
-  }
